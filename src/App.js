9a1f23e2715476932f8bda9710d006c244f88cc5
Harsh <Harsh.lakhanpal3@gmail.com>
2023-07-02 12:15:34 +0530

    This  is V1 of Codepen-Clone Project Using Reactjs

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+
+import Home from './Components/Home';
+import DataProvider from './Context/DataProvider';
+
+function App() {
+  return (
+    <DataProvider>
+      <Home />
+    </DataProvider>
+    
+  );
+}
+
+export default App;
